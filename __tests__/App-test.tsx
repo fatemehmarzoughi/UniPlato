277c42c67415bb8eb069ad41ca58a39bef09d2db
Saba <fatemeh.marzoughi18@gmail.com>
2022-08-07 22:48:44 +0430

    added tests for components

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,16 +1,28 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import * as React from "react";
+import renderer from "react-test-renderer";
+import Card from "../src/components/card";
+import { Post } from "../src/core/types";
+import Button from '../src/components/button';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-import { getAllPosts } from '../src/actions';
-import Router from '../src/route';
-
-it('renders correctly', () => {
-  // const result = Router.iconFiltering('', true);
-  // expect(result).toReturn();
+it(`renders Card component correctly with the post's details informations`, () => {
+  const item: Post = {
+    id: 0,
+    title: 'title',
+    description: 'description',
+    image: 'imageURL'
+  }
+  const CardSnapShot = renderer.create(<Card item={item} />);
+  expect(CardSnapShot).toMatchSnapshot();
+  expect(CardSnapShot.root.props.item.id).toBe(0);
+  expect(CardSnapShot.root.props.item.title).toBe('title');
+  expect(CardSnapShot.root.props.item.description).toBe('description');
+  expect(CardSnapShot.root.props.item.image).toBe('imageURL');
 });
 
-// Actions, Reducers, SnapShots
-// Animation
+it(`renders Button component correctly with the props's details informations`, () => {
+
+  const buttonSnapShot = renderer.create(<Button buttonText="buttonText" onPress={() => {}} />);
+  expect(buttonSnapShot).toMatchSnapshot();
+  expect(buttonSnapShot.root.props.buttonText).toBe('buttonText')
+
+});
